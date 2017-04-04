e3d966ff7045f68c2fe3254027187d2f8dbe4fe1
Jerne <jerne.vingerhoets@gmail.com>
2017-04-04 16:48:45 +0200

    V2
    added time calc

--- sorting/Sorting.java
+++ sorting/Sorting.java
@@ -7,9 +7,10 @@ public class Sorting {
 
 	public static Random random = new Random();
 	public static int randomInt;
-	static int[] arr = new int[15]; //The array we will be sorting, has a predifined length of 15, this ofcourse can be changed later
+	static int[] arr = new int[100000]; //The array we will be sorting, has a predifined length of 15, this ofcourse can be changed later
 	public static boolean isSorted = false; //Is true when the array is sorted
 	public static int temp;
+	public static long start = System.currentTimeMillis();
 	
 	//static int arr[] =  new int[] {15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 	
@@ -18,6 +19,13 @@ public class Sorting {
 		printArray(arr);
 		sortArray(arr);
 		printArray(arr);
+		calcTime();
+	}
+
+	private static void calcTime() {
+		long end = System.currentTimeMillis();
+		System.out.println("Sorting took: " + (end-start) + " milliseconds.");
+		
 	}
 
 	private static void printArray(int[] array) { //Method to print the array in a readable fashion ([I@6d06d69c) is just rubbish
@@ -49,10 +57,11 @@ public class Sorting {
 
 	private static void generateArray(int[] array) {
 		for(int i = 0; i < array.length; i++){ //filling the array
-			randomInt = random.nextInt(10000); //get a random number ranging from 0-1000
+			randomInt = random.nextInt(100000); //get a random number ranging from 0-1000
 			array[i] = randomInt;
 		}
 		
 	}
+	
 
 }
